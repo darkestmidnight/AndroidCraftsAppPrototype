6537cfef1eb8199cc9e019f6ac5fe3815ae3d021
Elly Richardson <elly.richardson18@gmail.com>
2018-10-12 00:54:50 -0500

    Added functionality in MainActivity and added SetupPage

--- app/src/main/java/com/example/android/androidcraftsappprototype/MainActivity.java
+++ app/src/main/java/com/example/android/androidcraftsappprototype/MainActivity.java
@@ -1,13 +1,65 @@
 package com.example.android.androidcraftsappprototype;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.content.Intent;
+import android.net.Uri;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    ImageButton AboutBtn, HomeSettingsBtn;
+    String url = "http://www.google.com";
+    Button HomeBeginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Example of a call to a native method
+        //TextView tv = (TextView) findViewById(R.id.sample_text);
+        //tv.setText(stringFromJNI());
+
+        AboutBtn = (ImageButton) findViewById(R.id.HomePurposeBtn);
+        AboutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
+
+        /*HomeBeginBtn = (Button) findViewById(R.id.HomeBeginBtn);
+        HomeBeginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SetupPage.class));
+            }
+        });*/
+        /*HomeSettingsBtn = (ImageButton) findViewById(R.id.HomeSettingsBtn);
+        HomeSettingsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Settings.class));
+            }
+        });*/
     }
+
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    //public native String stringFromJNI();
 }
---
+++ app/src/main/java/com/example/android/androidcraftsappprototype/SetupPage.java
@@ -0,0 +1,123 @@
+package com.example.android.androidcraftsappprototype;
+
+import android.content.DialogInterface;
+import android.media.Image;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Button;
+import android.content.Intent;
+import android.widget.ScrollView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class SetupPage extends AppCompatActivity {
+
+    // variables for home and setting up
+    ImageButton SetupHomeBtn;
+    //Button SetupAcptBtn;
+
+    // variables for multiple selection
+    ArrayAdapter<CharSequence> adapter;
+    ListView ListView1;
+    protected Button SetupAcceptBtn;
+    protected CharSequence[] receivers = {
+            "Receiver1", "Receiver2", "Receivers3"};
+    protected ArrayList<CharSequence> SelectedOptions = new ArrayList<>();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setup_page);
+
+        // to go to the next page
+        SetupHomeBtn = (ImageButton) findViewById(R.id.SetupHomeBtn);
+        SetupHomeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SetupPage.this, MainActivity.class));
+            }
+        });
+
+        // to process the whole selection of categories for the app thing
+        ListView1 = (ListView) findViewById(R.id.SetupListView);
+        SetupAcceptBtn = (Button) findViewById(R.id.SetupAcceptBtn);
+        SetupAcceptBtn.setOnClickListener((View.OnClickListener) this);
+
+        // string[] of my array
+        adapter = new ArrayAdapter<CharSequence>(this,
+                android.R.layout.simple_list_item_multiple_choice, SelectedOptions);
+
+        ListView1.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+        ListView1.setAdapter(adapter);
+
+        //scrollView1.setChoiceMode - disregard
+
+        // To go back one page
+        //SetupAcptBtn = (Button) findViewById(R.id.SetupAcceptBtn);
+        /*SetupAcceptBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Setup_Page.this, begin.class));
+            }
+        });*/
+    }
+
+    //@Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.SetupAcceptBtn:
+                showSetupAcceptDialog();
+                break;
+            default:
+                break;
+        }
+    }
+
+    protected void onChangeSelectedOptions() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (CharSequence receivers : SelectedOptions) {
+            stringBuilder.append(receivers + ",");
+        }
+        adapter.notifyDataSetChanged();
+    }
+
+    protected void showSetupAcceptDialog() {
+        boolean[] checkedOptions = new boolean[receivers.length];
+        int count = receivers.length;
+
+        for (int i = 0; i < count; i++) {
+            checkedOptions[i] = SelectedOptions.contains(receivers[i]);
+        }
+
+        DialogInterface.OnMultiChoiceClickListener OptionsDialogListener = new DialogInterface.OnMultiChoiceClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                if (isChecked)
+                    SelectedOptions.add(receivers[which]);
+                else
+                    SelectedOptions.remove(receivers[which]);
+
+                onChangeSelectedOptions();
+            }
+        };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Select Options");
+        builder.setMultiChoiceItems(receivers, checkedOptions, OptionsDialogListener);
+        builder.setCancelable(false);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.dismiss();
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+}
