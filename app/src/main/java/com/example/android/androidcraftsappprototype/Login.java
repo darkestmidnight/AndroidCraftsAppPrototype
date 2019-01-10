9254af8468637a920c5b1209f6afc053ff53f269
Elly Richardson <elly.richardson18@gmail.com>
2019-01-09 21:42:54 -0600

    Firebase removed. Setting up oauth2 for my REST API and mobile app connection

--- app/src/main/java/com/example/android/androidcraftsappprototype/Login.java
+++ app/src/main/java/com/example/android/androidcraftsappprototype/Login.java
@@ -1,5 +1,6 @@
 package com.example.android.androidcraftsappprototype;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
@@ -8,6 +9,9 @@ import android.view.View;
 import android.content.Intent;
 
 import android.util.Log;
+
+import net.smartam.leeloo.client.request.OAuthClientRequest;
+import net.smartam.leeloo.common.exception.OAuthSystemException;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -41,15 +45,34 @@ public class Login extends AppCompatActivity {
                 String strUserName = uUserName.getText().toString();
                 String strPassWord = uPassWord.getText().toString();
 
+                // To authorize oauth for requesting access token
+                OAuthClientRequest request = null;
+
+                try {
+                    // sends authorization request with credentials to the server
+                    request = OAuthClientRequest
+                            .authorizationLocation("http://192.168.0.18:8000/auth/authorize")
+                            .setClientId("mNUNXI4MinYyVvsRi1VPiv7ltrZWBOzr3OfnHJa1").setRedirectURI("http://oauthresponse")
+                            .buildQueryMessage();
+                } catch (OAuthSystemException e) {
+                    Log.d("TAG", "OAuth request failed", e);
+                }
+
+                // authentication service requires &reponse_type=code
+                Intent intent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse(request.getLocationUri() + "&response_type=code"));
+                startActivity(intent);
+
                 // API url duh
                 String APIUrl = "http://192.168.0.18:8000/token-auth/";
 
                 // If the user is authenticated, then transfer to the MainActivity page
-                if (APIAuthentication(strUserName, strPassWord, APIUrl)){
+                /*if (APIAuthentication(strUserName, strPassWord, APIUrl)){
                     startActivity(new Intent(Login.this, Posts.class));
-                }
+                }*/
             }
         });
+
 
         RegButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,6 +82,20 @@ public class Login extends AppCompatActivity {
             }
         });
 
+    }
+
+    // to handle authorization for the redirect_url
+    @Override
+    protected void onNewIntent(Intent intent)
+    {
+        super.onNewIntent(intent);
+        Uri uri = intent.getData();
+
+        if (uri != null && uri.toString()
+                .startsWith("http://oauthresponse"))
+        {
+            String code = uri.getQueryParameter("code");
+        }
     }
 
     private boolean APIAuthentication(String un, String pw, String url){
@@ -72,7 +109,7 @@ public class Login extends AppCompatActivity {
             postData.put("password", pw);
 
             // Putting the data to be posted in the Django API
-            AuthHelper.execute(url, postData.toString());
+            //AuthHelper.execute(url, postData.toString());
 
             return true;
         } catch (JSONException e) {
