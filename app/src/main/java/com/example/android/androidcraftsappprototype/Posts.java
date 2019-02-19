2f15569a9cf9cd1ecfa4809519d2ce45eb002442
Elly Richardson <elly.richardson18@gmail.com>
2019-02-18 23:40:26 -0600

    Posting Algorithm Fixed

--- app/src/main/java/com/example/android/androidcraftsappprototype/Posts.java
+++ app/src/main/java/com/example/android/androidcraftsappprototype/Posts.java
@@ -24,8 +24,6 @@ public class Posts extends AppCompatActivity {
     @Override
     protected void onResume(){
         super.onResume();
-        PostsDetails postDetailsHelper = new PostsDetails();
-        //postDetailsHelper.ListPosts();
     }
 
     @Override
@@ -35,8 +33,6 @@ public class Posts extends AppCompatActivity {
 
         postsDoneBtn = (Button) findViewById(R.id.PostsDoneButton);
         postsSect = (TextView) findViewById(R.id.PostsSection);
-
-        PostsDetails postDetailsHelper = new PostsDetails();
 
         PostsHelper = new WSAdapter().new SendPostsRequest(this, this);
         PostsHelper.execute("http://192.168.0.18:8000/api/profile/");
@@ -53,69 +49,29 @@ public class Posts extends AppCompatActivity {
         });
     }
 
-    public class PostsDetails {
-        //String post_title, post_content;
-        ArrayList<Integer> post_id = new ArrayList<Integer>();
-        ArrayList<String> post_title = new ArrayList<String>();
-        ArrayList<String> post_content = new ArrayList<String>();
+    public class PostsAttributes {
+        int post_id;
+        String post_title;
+        String post_content;
 
-        boolean isPDCalled;
-
-
-
-        // sets if Post details are called
-
-        // checks if postsDetails functions are called for AsyncTask
-        boolean getIsPDCalled(){
-            return isPDCalled;
+        // sets values for the posts
+        PostsAttributes(int p_id, String p_title, String p_content) {
+            this.post_id = p_id;
+            this.post_title = p_title;
+            this.post_content = p_content;
         }
 
-        // calls the execute for AsyncTask
-        private void callPostDetails(String theurl){
-            //PostsHelper = new WSAdapter().new SendPostsRequest(this);
-            // executes AsyncTask
-            PostsHelper.execute(theurl);
-        }
-
-        // sets values for the posts arrays
-        public void setPost(int p_id, String p_title, String p_content) {
-            this.post_id.add(p_id);
-            this.post_title.add(p_title);
-            this.post_content.add(p_content);
-        }
-
-        public ArrayList<Integer> getPostID() {
+        public int getPostId() {
             return this.post_id;
         }
 
-        public ArrayList<String> getPostTitle() {
+        public String getPostTitle() {
             return this.post_title;
         }
 
-        public ArrayList<String> getPostContent() {
+        public String getPostContent() {
             return this.post_content;
         }
 
-        // Lists the posts from the database
-        public void ListPosts() {
-            /////////// add functionality if a post was deleted and was clicked
-            postsSect = (TextView) findViewById(R.id.PostsSection);
-
-            int lastFrJSONArray = getPostID().size() - 1;
-
-            // outputs the id of the very first post, something to put to the textview
-            postsSect.setText("id: " + getPostID().get(0) + "\n");
-            for (int i = lastFrJSONArray; i >= 0; i--)
-            {
-                // appending the titles and contents of the current post
-                postsSect.append("title: " + getPostTitle().get(i) + "\n");
-                postsSect.append("content: " + getPostContent().get(i) + "\n");
-
-                // if this is the last post, then don't need to append id for the next post.
-                if (i != 0) {
-                    postsSect.append("id: " + getPostID().get(i) + "\n");
-                }
-            }
-        }
     }
 }
--- app/src/main/java/com/example/android/androidcraftsappprototype/WSAdapter.java
+++ app/src/main/java/com/example/android/androidcraftsappprototype/WSAdapter.java
@@ -15,6 +15,7 @@ import java.net.URL;
 import java.io.DataOutputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 import android.util.Log;
 
@@ -129,19 +130,10 @@ public class WSAdapter {
             // to save the Access Token from the API
             try {
                 JSONObject pJObject = new JSONObject(result);
-
                 PrefEditor.putString(accessToken, pJObject.getString("access_token"));
                 PrefEditor.apply();
-                // algorithm for parsing the JSONArray from the Django REST API
-                /*for (int i = 0; i < pJObjArray.length(); i++) {
-                    // puts the current iterated JSON object from the array to another temporary object
-                    JSONObject pJObj_data = pJObjArray.getJSONObject(i);
-                    PrefEditor.putString(accessToken, pJObj_data.getString("access_token"));
-                    PrefEditor.apply();
-                }*/
 
             } catch (JSONException e) {
-                //Toast.makeText(JSonActivity.this, e.toString(), Toast.LENGTH_LONG).show();
                 Log.d("Json","Exception = "+e.toString());
             }
         }
@@ -211,11 +203,12 @@ public class WSAdapter {
             // expecting a response code fro my server upon receiving the POST data
             Log.e("TAG", result);
 
-            // gets the JSON files stored in the posts details class from Posts Activity
-            Posts.PostsDetails postsHelper = new Posts().new PostsDetails();
 
             // retrieves the context passed
             Context context = mPostReference.get();
+
+            // gets the JSON files stored in the posts details class from Posts Activity
+            ArrayList<Posts.PostsAttributes> postsToShow = new ArrayList<Posts.PostsAttributes>();
 
             // For posts
             try {
@@ -225,8 +218,9 @@ public class WSAdapter {
                 for (int i = 0; i < pJObjArray.length(); i++) {
                     // puts the current iterated JSON object from the array to another temporary object
                     JSONObject pJObj_data = pJObjArray.getJSONObject(i);
-                    // inputs necesarry elements to the ListPosts function
-                    postsHelper.setPost(pJObj_data.getInt("id"), pJObj_data.getString("post_title"), pJObj_data.getString("post_content"));
+                    // inputs necesarry elements for the PostAttributes
+                    Posts.PostsAttributes postAttr = new Posts().new PostsAttributes(pJObj_data.getInt("id"), pJObj_data.getString("post_title"), pJObj_data.getString("post_content"));
+                    postsToShow.add(postAttr);
                 }
 
             } catch (JSONException e) {
@@ -237,23 +231,17 @@ public class WSAdapter {
             // checks if context is not null before updating posts page
             if (context != null){
                 postsSect = (TextView) activity.findViewById(R.id.PostsSection);
+                StringBuilder postsString = new StringBuilder();
 
-
-                int lastFrJSONArray = postsHelper.getPostID().size() - 1;
-
-                // outputs the id of the very first post, something to put to the textview
-                postsSect.setText("id: " + postsHelper.getPostID().get(lastFrJSONArray - 2) + "\n");
-                for (int i = lastFrJSONArray; i >= 0; i--)
-                {
-                    // appending the titles and contents of the current post
-                    postsSect.append("title: " + postsHelper.getPostTitle().get(i) + "\n");
-                    postsSect.append("content: " + postsHelper.getPostContent().get(i) + "\n");
-
-                    // if this is the last post, then don't need to append id for the next post.
-                    if (i != 0) {
-                        postsSect.append("id: " + postsHelper.getPostID().get(i) + "\n");
-                    }
+                for (int i = 0; i < postsToShow.size(); i++) {
+                    postsString.append(postsToShow.get(i).getPostId() + "\n");
+                    postsString.append(postsToShow.get(i).getPostTitle() + "\n");
+                    postsString.append(postsToShow.get(i).getPostContent() + "\n");
+                    //postsString.append("\n");
                 }
+
+                // Outputs the posts String
+                postsSect.setText(postsString);
             }
 
         }
